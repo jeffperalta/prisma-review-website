c1d4f00a697f157e7d59b393f5945fa2b6896267
Jeff Peralta <jeff@tsscore.com>
2021-01-05 17:01:12 +0800

    GraphQL: Query Operation Arguments

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,12 +1,55 @@
 const Query = {
     users(parent, args, { prisma }, info) {
-        return prisma.query.users(null, info)
+        const opArgs = {}
+
+        if(args.query) {
+            opArgs.where = {
+                OR: [{
+                    name_contains: args.query
+                },{
+                    email_contains: args.query
+                }]
+            }
+        }
+
+        return prisma.query.users(opArgs, info)
     },
     books(parent, args, { prisma }, info) {
-        return prisma.query.books(null, info)
+        const opArgs = {}
+
+        if (args.query) {
+            opArgs.where = {
+                OR: [{
+                    title_contains: args.query
+                }, {
+                    author_contains: args.query
+                }, {
+                    isbn: args.query
+                }]
+            }
+        }
+
+        return prisma.query.books(opArgs, info)
     },
     reviews(parent, args, { prisma }, info) {
-        return prisma.query.reviews(null, info)
+        const opArgs = {}
+
+        if (args.query) {
+            opArgs.where = {
+                OR: [{
+                    text_contains: args.query
+                }, {
+                    author: {
+                        name_contains: args.query
+                    }
+                }, {
+                    rating: +args.query
+                }]
+            }
+        }
+
+
+        return prisma.query.reviews(opArgs, info)
     }
 }
 
